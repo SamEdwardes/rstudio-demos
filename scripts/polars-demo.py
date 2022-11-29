a4d49c4377a358f92fbe92f40b3b4ab9926cd71f
SamEdwardes <edwardes.s@gmail.com>
2022-11-29 18:54:21 +0000

    Add vetiver example and polars example

---
+++ scripts/polars-demo.py
@@ -0,0 +1,25 @@
+import polars as pl
+
+# Eager API --------------------------------------------------------------------
+
+df = pl.read_csv("https://j.mp/iriscsv")
+print(df)
+
+print(
+    df
+    .filter(pl.col("sepal_length") > 5)
+    .groupby("species")
+    .agg(pl.all().sum())
+)
+
+# Lazy API ---------------------------------------------------------------------
+
+(
+    df
+    .lazy()
+    .filter(pl.col("sepal_length") > 5)
+    .groupby("species")
+    .agg(pl.all().sum())
+    .collect()
+)
+
---
+++ scripts/vetiver-train-and-deploy-model.py
@@ -0,0 +1,56 @@
+"""
+Train and deploy a linear model.
+
+- The model is trained using sklearn.
+- The fitted model from sklearn is passed into vetiver to create a vetiver model
+  ojbect.
+- Vetiver is used to pin the model weights to Connect.
+- Vetiver is used to deploy the model as a FastAPI API to Connect.
+"""
+
+import os
+import sys
+
+import pins
+import vetiver
+
+from vetiver.data import mtcars
+from sklearn.linear_model import LinearRegression
+from rsconnect.api import RSConnectServer
+
+# Fit a model ------------------------------------------------------------------
+
+lm = LinearRegression().fit(mtcars, mtcars["mpg"])
+username = os.getenv("USER")
+model_name = f"{username}/mtcars-linear-model"
+
+v = vetiver.VetiverModel(
+    lm,
+    model_name=model_name,
+    save_ptype=True,
+    ptype_data=mtcars
+)
+
+# Pin model --------------------------------------------------------------------
+
+board = pins.board_rsconnect(
+    server_url=os.getenv("CONNECT_SERVER"),
+    api_key=os.getenv("CONNECT_API_KEY"),
+    allow_pickle_read=True
+)
+
+vetiver.vetiver_pin_write(board, v)
+
+# Deploy model -----------------------------------------------------------------
+
+connect_server = RSConnectServer(
+    url=os.getenv("CONNECT_SERVER"), 
+    api_key=os.getenv("CONNECT_API_KEY")
+)
+
+vetiver.deploy_rsconnect(
+    connect_server=connect_server,
+    board=board,
+    pin_name=model_name,
+    python=sys.executable,
+)
