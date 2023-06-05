92fd4b29a499703de353b9e8521f8be717e96a45
SamEdwardes <edwardes.s@gmail.com>
2023-06-05 14:49:26 +0000

    Add more items to safelist

--- scripts/clean-colorado.R
+++ scripts/clean-colorado.R
@@ -1,10 +1,11 @@
 library(dplyr)
 library(connectapi)
 
-client <- connect(server = "https://colorado.rstudio.com/rsc")
-sam_edawrdes_guid <- "d03a6b7a-c818-4e40-8ef9-84ca567f9671"
+client <- connect(server = "https://colorado.posit.co/rsc")
+sam_edwardes_guid <- "d03a6b7a-c818-4e40-8ef9-84ca567f9671"
 
-all_content <- get_content(client, owner_guid = sam_edawrdes_guid)
+all_content <- get_content(client, owner_guid = sam_edwardes_guid) |> 
+  filter(owner_guid == sam_edwardes_guid)
 
 white_list <- c(
   "Using VS Code in Workbench",
@@ -12,7 +13,9 @@ white_list <- c(
   "The Colorado Report",
   "Workbench Admin Guide (PREVIEW)",
   "Workbench User Guide (PREVIEW)",
-  "Global variable scope test"
+  "Global variable scope test",
+  "Sam's Shiny Shell",
+  "RMarkdown - Blastula - Connect Content Report"
 )
 
 content_to_delete <-
