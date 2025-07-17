8aa40cb282d58b14abafbf3e4fd3534cd30a551d
SamEdwardes <sam.edwardes@posit.co>
2025-07-17 16:27:03 +0000

    Update FastAPI penguins to use newer versions of Python packages and pullers instead of pandas

--- apis/fastapi-penguins/README.md
+++ apis/fastapi-penguins/README.md
@@ -10,33 +10,42 @@
 Create a virtual environment.
 
 ```bash
-python -m venv .venv
-source .venv/bin/activate
-python -m pip install --upgrade pip wheel setuptools
-python -m pip install -r requirements.txt
+uv sync
 ```
 
 Then run the app.
 
 ```bash
-uvicorn app:app --reload
+uv run uvicorn app:app --reload
 ```
 
 You could also call the API using `curl`:
 
 ```bash
-curl -X 'GET' \
-  'https://colorado.rstudio.com/rsc/demo-fastapi-penguins/penguins?sample_size=1' \
+CONNECT_SERVER='https://pub.current.posit.team'
+
+curl -L -X 'GET' \
+  "${CONNECT_SERVER}/public/fastapi-penguins/penguins?sample_size=1" \
   -H 'accept: application/json'
 ```
 
 If the API is private you will need to authenticate using your API key.
 
 ```bash
-curl -X 'GET' \
-  'https://colorado.rstudio.com/rsc/demo-fastapi-penguins/penguins?sample_size=1' \
+CONNECT_SERVER='https://pub.current.posit.team'
+
+curl -L -X 'GET' \
+  "${CONNECT_SERVER}/public/fastapi-penguins/penguins?sample_size=1" \
   -H 'accept: application/json' \
   -H "Authorization: Key ${CONNECT_API_KEY}"
+```
+
+Cleaner syntx with httpie:
+
+```bash
+uvx --from httpie https pub.current.posit.team/public/fastapi-penguins/penguins \
+  "Authorization:Key ${CONNECT_API_KEY}" \
+  sample_size==2
 ```
 
 ## Deployment
@@ -46,6 +55,8 @@ curl -X 'GET' \
 The app is automatically deployed to RStudio connect using git backed deployment. Make any changes to the code, then run the following:
 
 ```bash
+uv run export -o requirements.txt --no-hashes
+
 rsconnect write-manifest fastapi \
   --overwrite \
   --python .venv/bin/python \
@@ -60,6 +71,8 @@ rsconnect write-manifest fastapi \
 You can deploy the app using the rsconnect cli:
 
 ```bash
+uv run export -o requirements.txt --no-hashes
+
 rsconnect deploy fastapi \
   --python .venv/bin/python \
   --entrypoint app:app \
--- apis/fastapi-penguins/app.py
+++ apis/fastapi-penguins/app.py
@@ -1,33 +1,24 @@
 import datetime as dt
-from typing import List, Optional
 
-import pandas as pd
+import polars as pl
 from fastapi import FastAPI
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
-class PenguinsBase(BaseModel):
-    @validator("*")
-    def change_nan_to_none(cls, v, field):
-        if field.outer_type_ is float and pd.isna(v):
-            return None
-        return v
+class Penguin(BaseModel):
+    species: str | None = None
+    island: str | None = None
+    bill_length_mm: float | None = None
+    bill_depth_mm: float | None = None
+    flipper_length_mm: float | None = None
+    body_mass_g: float | None = None
+    sex: str | None = None
+    year: int | None = None
 
 
-class Penguin(PenguinsBase):
-    species: Optional[str]
-    island: Optional[str]
-    bill_length_mm: Optional[float]
-    bill_depth_mm: Optional[float]
-    flipper_length_mm: Optional[float]
-    body_mass_g: Optional[float]
-    sex: Optional[str]
-    year: Optional[int]
-
-
-class PenguinRaw(PenguinsBase):
+class PenguinRaw(BaseModel):
     study_name: str = Field(alias="studyName")
     sample_number: int = Field(alias="Sample Number")
     species: str = Field(alias="Species")
@@ -36,14 +27,14 @@ class PenguinRaw(PenguinsBase):
     stage: str = Field(alias="Stage")
     individual_id: str = Field(alias="Individual ID")
     clutch_completion: str = Field(alias="Clutch Completion")
-    date_egg: Optional[dt.date] = Field(alias="Date Egg")
-    culmen_length_mm: Optional[float] = Field(alias="Culmen Length (mm)")
-    culmen_depth_mm: Optional[float] = Field(alias="Culmen Depth (mm)")
-    flipper_length_mm: Optional[float] = Field(alias="Flipper Length (mm)")
-    body_mass_g: Optional[float] = Field(alias="Body Mass (g)")
+    date_egg: dt.date | None = Field(default=None, alias="Date Egg")
+    culmen_length_mm: float | None = Field(default=None, alias="Culmen Length (mm)")
+    culmen_depth_mm: float | None = Field(default=None, alias="Culmen Depth (mm)")
+    flipper_length_mm: float | None = Field(default=None, alias="Flipper Length (mm)")
+    body_mass_g: float | None = Field(default=None, alias="Body Mass (g)")
     sex: str = Field(alias="Sex")
-    delta_15_n_o_oo: Optional[float] = Field(alias="Delta 15 N (o/oo)")
-    delta_13_c_o_oo: Optional[float] = Field(alias="Delta 13 C (o/oo)")
+    delta_15_n_o_oo: float | None = Field(default=None, alias="Delta 15 N (o/oo)")
+    delta_13_c_o_oo: float | None = Field(default=None, alias="Delta 13 C (o/oo)")
 
 
 # @app.get("/", response_class=RedirectResponse)
@@ -51,23 +42,21 @@ class PenguinRaw(PenguinsBase):
 #     return RedirectResponse("/docs")
 
 
-@app.get("/penguins", response_model=List[Penguin])
-def penguins(sample_size: Optional[int] = None):
+@app.get("/penguins", response_model=list[Penguin])
+def penguins(sample_size: int | None = None):
     url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/main/inst/extdata/penguins.csv"
-    penguins_df = pd.read_csv(url)
+    penguins_df = pl.read_csv(url)
     if sample_size:
         penguins_df = penguins_df.sample(sample_size)
-    penguins_response = [Penguin(**i) for i in penguins_df.to_dict(orient="records")]
+    penguins_response = [Penguin(**i) for i in penguins_df.to_dicts()]
     return penguins_response
 
 
-@app.get("/raw_penguins", response_model=List[PenguinRaw])
-def raw_penguins(sample_size: Optional[int] = None):
+@app.get("/raw_penguins", response_model=list[PenguinRaw])
+def raw_penguins(sample_size: int | None = None):
     url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/main/inst/extdata/penguins.csv"
-    penguins_raw_df = pd.read_csv(url)
+    penguins_raw_df = pl.read_csv(url)
     if sample_size:
         penguins_raw_df = penguins_raw_df.sample(sample_size)
-    penguins_raw_response = [
-        PenguinRaw(**i) for i in penguins_raw_df.to_dict(orient="records")
-    ]
+    penguins_raw_response = [PenguinRaw(**i) for i in penguins_raw_df.to_dicts()]
     return penguins_raw_response
