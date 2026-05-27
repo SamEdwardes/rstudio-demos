19671f1ef5d39f3aafc49044748cb1dce693456b
SamEdwardes <sam.edwardes@posit.co>
2026-05-27 15:51:57 +0000

    Update FastAPI penguins

--- apis/fastapi-penguins/app.py
+++ apis/fastapi-penguins/app.py
@@ -10,14 +10,14 @@ app = FastAPI()
 
 
 class Penguin(BaseModel):
-    species: str | None = None
-    island: str | None = None
-    bill_length_mm: float | None = None
-    bill_depth_mm: float | None = None
-    flipper_length_mm: float | None = None
-    body_mass_g: float | None = None
-    sex: str | None = None
-    year: int | None = None
+    species: str | None = Field(default=None)
+    island: str | None = Field(default=None)
+    bill_length_mm: float | None = Field(default=None)
+    bill_depth_mm: float | None = Field(default=None)
+    flipper_length_mm: float | None = Field(default=None)
+    body_mass_g: float | None = Field(default=None)
+    sex: str | None = Field(default=None)
+    year: int | None = Field(default=None)
 
 
 class PenguinRaw(BaseModel):
@@ -48,7 +48,7 @@ class PenguinRaw(BaseModel):
 def penguins(sample_size: int | None = None):
     logger.info(f"Server /penguins endpoint - {sample_size=}, {socket.gethostname()=}")
     url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/main/inst/extdata/penguins.csv"
-    penguins_df = pl.read_csv(url)
+    penguins_df = pl.read_csv(url, null_values=["NA"])
     if sample_size:
         penguins_df = penguins_df.sample(sample_size)
     penguins_response = [Penguin(**i) for i in penguins_df.to_dicts()]
@@ -61,8 +61,8 @@ def raw_penguins(sample_size: int | None = None):
         f"Server /raw_penguins endpoint - {sample_size=}, {socket.gethostname()=}"
     )
     url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/main/inst/extdata/penguins.csv"
-    penguins_raw_df = pl.read_csv(url)
+    penguins_raw_df = pl.read_csv(url, null_values=["NA"])
     if sample_size:
-        penguins_raw_df = penguins_raw_df.sample(sample_size)
+        penguins_raw_df = penguins_raw_df.sample(sample_size, null_values=["NA"])
     penguins_raw_response = [PenguinRaw(**i) for i in penguins_raw_df.to_dicts()]
     return penguins_raw_response
